46b927eb56030246b59db3ae9e6ec2e8c71cdb42
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-16 06:49:24 -0400

    add types in userslice

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,8 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const initialState = {
+type User = {
+  id: number;
+  name: string;
+};
+
+type InitialState = {
+  loading: boolean;
+  users: User[];
+  error: string;
+};
+
+const initialState: InitialState = {
   loading: false,
   users: [],
   error: "",
@@ -19,19 +30,23 @@ const fetchUsers = createAsyncThunk("user/fetchUsers", async () => {
 const userSlice = createSlice({
   name: "user",
   initialState,
+  reducers: {},
   extraReducers: (builder) => {
     builder.addCase(fetchUsers.pending, (state) => {
       state.loading = true;
     });
-    builder.addCase(fetchUsers.fulfilled, (state, action) => {
-      state.loading = false;
-      state.users = action.payload;
-      state.error = "";
-    });
+    builder.addCase(
+      fetchUsers.fulfilled,
+      (state, action: PayloadAction<User[]>) => {
+        state.loading = false;
+        state.users = action.payload;
+        state.error = "";
+      }
+    );
     builder.addCase(fetchUsers.rejected, (state, action) => {
       state.loading = false;
       state.users = [];
-      state.error = action.error.message;
+      state.error = action.error.message || "Something went wrong";
     });
   },
 });
