2de1041f7e9ad4ac7031299d287d36e958009eb9
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-17 08:04:11 -0400

    wrap root component in redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,17 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./app/store";
+import persistStore from "redux-persist/es/persistStore";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
