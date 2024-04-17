d7905f50b99ff5194b08d7c4c2e9ba1ffe2c738d
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-17 07:59:42 -0400

    add redux-persist configs in the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { createLogger } from "redux-logger";
 import cakeReducer from "../features/cake/cakeSlice";
 import icecreamReducer from "../features/icecream/icecreamSlice";
 import userReducer from "../features/user/userSlice";
 
+//Redux persist setup
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+
 const logger = createLogger();
 
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: icecreamReducer,
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    cake: cakeReducer,
-    iceCream: icecreamReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
